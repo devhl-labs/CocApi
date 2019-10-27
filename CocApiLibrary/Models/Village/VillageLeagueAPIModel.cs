417e1915e4cfa1c6ea4c3f4edbe193adb3e2e10b
devhl-labs <shawnkanyer@gmail.com>
2019-10-27 17:00:36 -0400

    renamed league objects

---
+++ CocApiLibrary/Models/Village/VillageLeagueAPIModel.cs
@@ -0,0 +1,43 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
+
+namespace CocApiLibrary.Models
+{
+    public class VillageLeagueAPIModel
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Name { get; set; } = string.Empty;
+
+
+        private VillageLeagueIconUrlAPIModel? _iconUrl;
+        
+        [JsonPropertyName("IconUrls")]
+        [ForeignKey(nameof(LeagueIconUrlId))]
+        public virtual VillageLeagueIconUrlAPIModel? LeagueIconUrl
+        {
+            get
+            {
+                return _iconUrl;
+            }
+        
+            set
+            {
+                _iconUrl = value;
+
+                if (_iconUrl != null)
+                {
+                    LeagueIconUrlId = _iconUrl.Id;
+                }
+                else
+                {
+                    LeagueIconUrlId = null;
+                }
+            }
+        }
+
+        public string? LeagueIconUrlId { get; set; } = string.Empty;
+    }
+}
