fbf29debc5d10ad798e4af53fe6f613f1ba85793
devhl-labs <shawnkanyer@gmail.com>
2020-05-28 11:05:17 -0400

    starting a new branch

---
+++ CocApiLibrary/Models/Cache/CachedClan.cs
@@ -0,0 +1,16 @@
+﻿//using Dapper.SqlWriter;
+//using System;
+//using System.Collections.Generic;
+//using System.Text;
+
+//namespace devhl.CocApi.Models.Cache
+//{
+//    internal class CachedClan : DBObject
+//    {
+//        public string ClanTag { get; set; } = string.Empty;
+
+//        public string Json { get; set; } = string.Empty;
+
+//        public bool Track { get; set; }
+//    }
+//}
