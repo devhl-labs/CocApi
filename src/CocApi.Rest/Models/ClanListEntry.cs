ad54a198cd285bfb9080dbd4cab3a7e55ba1f5f5
devhl <shawnkanyer@gmail.com>
2022-12-17 18:06:52 -0500

    reverted the warLeague hotfix due to the api bug being fixed

--- src/CocApi.Rest/Models/ClanListEntry.cs
+++ src/CocApi.Rest/Models/ClanListEntry.cs
@@ -56,8 +56,8 @@ namespace CocApi.Rest.Models
 #pragma warning disable CS0472 // The result of the expression is always the same since a value of this type is never equal to 'null'
 #pragma warning disable CS8073 // The result of the expression is always the same since a value of this type is never equal to 'null'
 
-            //if (warLeague == null)
-            //    throw new ArgumentNullException("warLeague is a required property for ClanListEntry and cannot be null.");
+            if (warLeague == null)
+                throw new ArgumentNullException("warLeague is a required property for ClanListEntry and cannot be null.");
 
             if (requiredTrophies == null)
                 throw new ArgumentNullException("requiredTrophies is a required property for ClanListEntry and cannot be null.");
