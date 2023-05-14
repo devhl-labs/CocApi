ceaec7afc9fceb7187837e05adb906fc37baa3ce
devhl <shawnkanyer@gmail.com>
2023-05-14 13:44:16 -0400

    minor refactors

--- docs/scripts/generate.ps1
+++ docs/scripts/generate.ps1
@@ -91,33 +91,6 @@ $membersConverter = @"
 
 "@
 
-$clanWarMemberConstructor = @"
-        /// <summary>
-        /// Initializes a new instance of the <see cref="ClanWarMember" /> class.
-        /// </summary>
-        /// <param name="mapPosition">mapPosition</param>
-        /// <param name="name">name</param>
-        /// <param name="opponentAttacks">opponentAttacks</param>
-        /// <param name="tag">tag</param>
-        /// <param name="townhallLevel">townhallLevel</param>
-        /// <param name="attacks">attacks</param>
-        /// <param name="bestOpponentAttack">bestOpponentAttack</param>
-        [JsonConstructor]
-        internal ClanWarMember(int mapPosition, string name, int opponentAttacks, string tag, int townhallLevel, List<ClanWarAttack>? attacks = default, ClanWarAttack? bestOpponentAttack = default)
-        {
-            MapPosition = mapPosition;
-            Name = name;
-            OpponentAttacks = opponentAttacks;
-            Tag = tag;
-            TownhallLevel = townhallLevel;
-            Attacks = attacks;
-            BestOpponentAttack = bestOpponentAttack;
-            OnCreated();
-        }
-
-
-"@
-
 $apiKey = @"
                     List<TokenBase> tokenBaseLocalVars = new List<TokenBase>();
 
@@ -310,7 +283,7 @@ foreach ($file in $allCodeFiles)
 
     if ($file.name -eq "ClanWarMember.cs"){
         $content = $content.Replace($mapPosition, "")
-        $content = $content.Replace($clanWarMemberConstructor, "")
+        $content = $content.Replace("MapPosition = mapPosition;", "RosterPosition = mapPosition; // this is intentional. The MapPosition will be caculated in ClanWar#OnCreated")
     }
 
     if ($file.name -eq "ClanWar.cs"){
--- src/CocApi.Rest/Models/ClanWarMember.Manual.cs
+++ src/CocApi.Rest/Models/ClanWarMember.Manual.cs
@@ -9,30 +9,6 @@ namespace CocApi.Rest.Models
 
 
         /// <summary>
-        /// Initializes a new instance of the <see cref="ClanWarMember" /> class.
-        /// </summary>
-        /// <param name="mapPosition">mapPosition</param>
-        /// <param name="name">name</param>
-        /// <param name="opponentAttacks">opponentAttacks</param>
-        /// <param name="tag">tag</param>
-        /// <param name="townhallLevel">townhallLevel</param>
-        /// <param name="attacks">attacks</param>
-        /// <param name="bestOpponentAttack">bestOpponentAttack</param>
-        [JsonConstructor]
-        internal ClanWarMember(int mapPosition, string name, int opponentAttacks, string tag, int townhallLevel, List<ClanWarAttack>? attacks = default, ClanWarAttack? bestOpponentAttack = default)
-        {
-            RosterPosition = mapPosition; // this is intentional. The MapPosition will be caculated in ClanWar#OnCreated
-            Name = name;
-            OpponentAttacks = opponentAttacks;
-            Tag = tag;
-            TownhallLevel = townhallLevel;
-            Attacks = attacks;
-            BestOpponentAttack = bestOpponentAttack;
-            OnCreated();
-        }
-
-
-        /// <summary>
         /// Gets or Sets MapPosition
         /// </summary>
         [JsonPropertyName("mapPosition")]
