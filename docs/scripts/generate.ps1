63620a80cb85562f24350cb1fab00b7406d28e14
devhl <shawnkanyer@gmail.com>
2023-05-28 20:49:29 -0400

    version bump

--- docs/scripts/generate.ps1
+++ docs/scripts/generate.ps1
@@ -1,4 +1,4 @@
-$packageVersion = "2.5.5"
+$packageVersion = "2.5.6"
 $releaseNote = "Moved rest methods to CocApi.Rest. Now using automation to generate rest methods from openapi yaml."
 
 $properties = @(
